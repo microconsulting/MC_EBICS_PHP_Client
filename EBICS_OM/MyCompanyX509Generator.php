b13dababd2bad334bc12ecd8531cb4ac50d76371
moreasar <sarah.moreau@officemaker.ch>
2024-06-04 09:57:33 +0200

    Automate certificate generation
    Edit certificate thank to X509.json file

--- EBICS_OM/MyCompanyX509Generator.php
+++ EBICS_OM/MyCompanyX509Generator.php
@@ -7,25 +7,8 @@ use AndrewSvirin\Ebics\Models\X509\AbstractX509Generator;
 class MyCompanyX509Generator extends AbstractX509Generator
 {
     protected function getCertificateOptions() : array {
-        
-        return [
-             'subject' => [
-                'DN' => [
-                    'id-at-countryName' => 'FR',
-                    'id-at-stateOrProvinceName' => 'State',
-                    'id-at-localityName' => 'City',
-                    'id-at-organizationName' => 'Your company',
-                    'id-at-commonName' => 'yourwebsite.tld',
-                    ]
-                ],
-            //     'extensions' => [
-            //         'id-ce-subjectAltName' => [
-            //         'value' => [
-            //             'dNSName' => '*.yourwebsite.tld',
-            //         ]
-            //     ],
-            // ],
-        ];
+        $path = __DIR__ . '/_data/certificate/X509.json';
+        return json_decode(file_get_contents($path), true);
     }
 }
 
